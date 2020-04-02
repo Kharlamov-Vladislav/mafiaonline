f175150888a43e04b194469d73400503e09ced9a
Vladislav <48480239+Kharlamov-Vladislav@users.noreply.github.com>
2020-04-02 06:34:34 +0300

    fix doc

--- scripts/api_mafia.py
+++ scripts/api_mafia.py
@@ -1,15 +1,7 @@
 import requests
 import json
 def get_log(id_game):
-    """Возращает полную информацию о партии:
-    ----------------------------------------------------
-    ВХОДНЫЕ ПАРАМЕТРЫ:
-    id_game :: Целочисленное значение уникального индификатора партии
-    
-    ВОЗРАЩАЕТ:
-    Полный лог партии в формате словаря
-    """
-    
+
     url = f'https://www.mafiaonline.ru/api/api.php?action=log&param=log&id={id_game}'
     r = requests.get(url)
     status_code = r.status_code
