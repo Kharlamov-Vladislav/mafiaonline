be8cde8c50d1a99e0e9821265b485e01d82c2c22
Vladislav <48480239+Kharlamov-Vladislav@users.noreply.github.com>
2020-04-05 13:51:35 +0300

    add parse move players

--- scripts/mafia_tools.py
+++ scripts/mafia_tools.py
@@ -3,12 +3,14 @@ import re
 import numpy as np
 from bs4 import BeautifulSoup
 
-def time2seconds(array_time):
-    array_time = [array_time[0]] + array_time # Добавляем нулевое сообщение с которого идет отсчёт
-    array_time = list(map(lambda x: int(x.split(':')[0]) * 60 + int(x.split(':')[1]), array_time)) # Переводим mm:ss -> seconds
-    return np.cumsum((np.diff(array_time) % 3600))
-
 def parse_chat(chat):
+
+    def time2seconds(array_time):
+        array_time = [array_time[0]] + array_time # Добавляем нулевое сообщение с которого идет отсчёт
+        array_time = list(map(lambda x: int(x.split(':')[0]) * 60 + int(x.split(':')[1]), array_time)) # Переводим mm:ss -> seconds
+        return np.cumsum((np.diff(array_time) % 3600))
+        
+        
     curr_elem, prev_elem = '', ''
     array_messages, array_time = [], []
     
@@ -20,3 +22,41 @@ def parse_chat(chat):
                 array_messages.append(curr_elem.get_text())
     array_time = time2seconds(array_time) #Возращает список секунд прошедших для каждого сообщения относительного первого
     return list(zip(array_time, array_messages))
+
+def parse_summary(log):
+
+    def get_move(move):
+        decrypte_color = {'red' : 'move-maf', '#d5c32d' : 'move-positive'}
+        colors = 'red|#d5c32d'
+        r = re.findall(colors, move['style'])
+        if r:
+            return decrypte_color[r[0]]
+        return 'move-city'
+        
+        
+    CONST_UPDATE_CHAT = 3798821
+    new_chat = True if log['id'] > CONST_UPDATE_CHAT else False
+    
+    if new_chat:
+        types_move = ['move move-city', 'move move-maf', 'move move-positive', 'move move-doc', 'move move-man', 'move move-kom']
+        moves = BeautifulSoup(log['summary']).find_all('span', {'class' : types_move})
+        parse_moves = []
+        player_nicks = '|'.join(list(map(lambda player: player['nick'], log['players'])))
+        for move in moves:
+            msg = move.get_text()
+            nick_1, nick_2 = re.findall(player_nicks, msg)
+            parse_moves.append([move['class'][1], (nick_1, nick_2)])
+            
+            
+    else:
+        moves = BeautifulSoup(log['summary']).find_all('b', {'style' : True})
+        parse_moves = []
+        player_nicks = '|'.join(list(map(lambda player: player['nick'], log['players'])))
+        for move in moves:
+            msg = move.get_text()
+            nicks = re.findall(player_nicks, msg)
+            if len(nicks) == 2:
+                nick_1, nick_2 = nicks
+            parse_moves.append([get_move(move), (nick_1, nick_2)])
+
+    return parse_moves
